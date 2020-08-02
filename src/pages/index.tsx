e90ee38b5331510afc03f5ee884a72e25e723ab8
Laura Wenning <laura.r.wenning@gmail.com>
2020-08-02 00:59:51 +0000

    These didn't get included <_<

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,12 @@ export default function Index() {
   return (
     <Page>
       <div className="construction-message">
-        <h1>Woah, hey!</h1>
-        <h2>
+        <h1 className="text-center">Woah, hey!</h1>
+        <h2 className="text-center">
           It looks like a new portfolio is under construction!
           Our construction owls are hard at work, but we're not quite done yet. 
           We apologize for the inconvinence. 
-          If you would like to see Laura's old portfolio site, 
+          If you would like to see Laura's old portfolio site,&nbsp;
           <Link href="/old-version" passHref>you can check it out here!</Link>
         </h2>
       </div>
