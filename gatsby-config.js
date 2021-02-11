3cea31de17b0b2745b26d701c3357b49dfc0866b
Laura Wenning <laura.r.wenning@gmail.com>
2021-02-11 08:40:53 -0500

    Latest changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Laura's Portfolio`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@laurawenning`,
   },
   plugins: [
+    `@rennehir/gatsby-theme-personal-site`
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
